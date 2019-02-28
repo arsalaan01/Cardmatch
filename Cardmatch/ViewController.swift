26b82bbdbb702c6c865e6a9f76421d48393c19f3
Arsalaan <arsalaan0101@gmail.com>
2019-03-01 03:44:54 +0530

    day - 3

--- Cardmatch/ViewController.swift
+++ Cardmatch/ViewController.swift
@@ -23,14 +23,14 @@ class ViewController: UIViewController {
     
     @IBOutlet var cardButtons: [UIButton]!
     
-   
+    
     
     @IBAction func touchCard(_ sender: UIButton) {
         
         
         flipCount += 1
         if let cardNumber = cardButtons.index(of: sender) {
-           // flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+            // flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
             game.chooseCard(at: cardNumber)
             updateViewFromModel()
             
@@ -52,27 +52,27 @@ class ViewController: UIViewController {
                 button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)           }
         }
     }
-     var emojiChoices = ["🎃","👻","👑","🐬","🐙","🤡","🐠","🦋","❄️","🦄"]
+    var emojiChoices = ["🎃","👻","👑","🐬","🐙","🤡","🐠","🦋","❄️","🦄"]
     
     //var emoji = Dictionary<Int,String>()
     var emoji = [Int:String]()
     func emoji(for card: Card) ->String{
         /*if emoji[card.identifier] != nil{
-            return emoji[card.identifier]!
-        }else{
-            return "?"
-        } */
+         return emoji[card.identifier]!
+         }else{
+         return "?"
+         } */
         //same as above commented code
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
             
-                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-                //emoji[card.identifier] = emojiChoices[randomIndex]
-                emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            //emoji[card.identifier] = emojiChoices[randomIndex]
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
             
         }
         return emoji[card.identifier] ?? "?"
     }
-
- 
+    
+    
 }
 
